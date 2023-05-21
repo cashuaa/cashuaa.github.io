18ee0195e7c8c9cf306b3105c508c24232f81b8b
cashuaa <joshcash95@gmail.com>
2023-05-21 12:34:22 -0700

    complete revamp

--- testJavascript.js
+++ testJavascript.js
@@ -1,9 +1,16 @@
 let availableAircraftKeywords = [
-    'Night Bomber',
-    'Speed Canard',
-    'test',
-    'test1',
-    'joshua',
+    'NIGHT BOMBER',
+    'SPEED CANARD',
+    'OSPREY',
+    'B-17 FLYING FORTESS',
+    'B-52 STRATOFORTRESS',
+    'F-1 MIRRAGE',
+    'SUPER ETENDARD',
+    'JAGUAR',
+    'MIRRAGE 2000C',
+    'MIRRAGE 3',
+    'RAFALE',
+    'C-47 SKYTRAIN',
     ];
 
 let availableNavalKeywords = [
